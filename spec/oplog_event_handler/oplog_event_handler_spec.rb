e48f7ad3f69f6b98137f49763e49269f21c7429e
mathieulaporte <mlaporte@azendoo.com>
2013-02-15 09:19:45 +0100

    update specs

--- spec/oplog_event_handler/oplog_event_handler_spec.rb
+++ spec/oplog_event_handler/oplog_event_handler_spec.rb
@@ -6,7 +6,7 @@ describe "OplogEventHandler" do
     @log1 = { "ts"  => 354235435435435, "h"  => -1856809684451276882, "op"  => "u", "ns"  => "az_app_2_test.subjects", "o2"  => { "_id"  => BSON::ObjectId("50c21295caa8618d99001e56") }, "o"  => { "$addToSet"  => { "conversation_ids"  => BSON::ObjectId("50c21296caa8618d99001e6c") } } }
   end
 
-  subject { OplogEventHandler.new }
+  subject { class Test; include OplogEventHandler; end.new }
 
   describe "#extract_db_name(doc)" do
 
@@ -19,7 +19,7 @@ describe "OplogEventHandler" do
   describe "#extract_collection_name(doc)" do
 
     context "with a valid doc" do
-      it { OplogEventHandler.new.send(:extract_collection_name, @log1).should == 'subjects' }
+      it { subject.send(:extract_collection_name, @log1).should == 'subjects' }
     end
 
   end
@@ -27,23 +27,23 @@ describe "OplogEventHandler" do
   describe "#extract_operation(doc)" do
 
     context "for insert" do
-      it { OplogEventHandler.new.send(:extract_operation, @log1.merge({'op' => 'i'})).should == :insert }
+      it { subject.send(:extract_operation, @log1.merge({'op' => 'i'})).should == :insert }
     end
 
     context "for update" do
-      it { OplogEventHandler.new.send(:extract_operation, @log1.merge({'op' => 'u'})).should == :update }
+      it { subject.send(:extract_operation, @log1.merge({'op' => 'u'})).should == :update }
     end
 
     context "for delete" do
-      it { OplogEventHandler.new.send(:extract_operation, @log1.merge({'op' => 'd'})).should == :delete }
+      it { subject.send(:extract_operation, @log1.merge({'op' => 'd'})).should == :delete }
     end
 
     context "for command" do
-      it { OplogEventHandler.new.send(:extract_operation, @log1.merge({'op' => 'c'})).should == :dbcmd }
+      it { subject.send(:extract_operation, @log1.merge({'op' => 'c'})).should == :dbcmd }
     end
 
     context "for noop" do
-      it { OplogEventHandler.new.send(:extract_operation, @log1.merge({'op' => 'n'})).should == :noop }
+      it { subject.send(:extract_operation, @log1.merge({'op' => 'n'})).should == :noop }
     end
 
   end
